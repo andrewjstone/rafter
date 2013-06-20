c1101341be7d534f987d44e55001fe10d874ab84
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2013-06-20 16:35:34 -0400

    rafter_config is not a process callback module

--- src/rafter_consensus_sup.erl
+++ src/rafter_consensus_sup.erl
@@ -23,16 +23,11 @@ init([NameAtom, Me, Peers, StateMachine]) ->
                  {rafter_log, start_link, [LogName]},
                  permanent, 5000, worker, [rafter_log]},
 
-    ConfigName = name(NameAtom, "conf"),
-    ConfigServer = { rafter_config,
-                     {rafter_Config, start_link, [ConfigName]},
-                     permanent, 5000, worker, [rafter_config]},
-
     ConsensusFsm = { rafter_consensus_fsm,
                     {rafter_consensus_fsm, start_link, [NameAtom, Me, Peers, StateMachine]},
                     permanent, 5000, worker, [rafter_consensus_fsm]},
 
-    {ok, {{one_for_all, 5, 10}, [LogServer, ConfigServer, ConsensusFsm]}}.
+    {ok, {{one_for_all, 5, 10}, [LogServer, ConsensusFsm]}}.
 
 %% ===================================================================
 %% Private Functions 
